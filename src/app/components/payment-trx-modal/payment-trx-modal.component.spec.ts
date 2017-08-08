1635ede793ea9ceaa61a6bee21a4b7e50a415edd
Hassan Masroor <Hassan.Masroor@me.com>
2017-08-08 11:14:35 -0400

    initial test setup for paymentTrxModal completed

--- src/app/components/payment-trx-modal/payment-trx-modal.component.spec.ts
+++ src/app/components/payment-trx-modal/payment-trx-modal.component.spec.ts
@@ -1,16 +1,51 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
 
+import { AppStateActions } from '../../actionHandlers/appState.actions';
+import { NotificationActions } from '../../actionHandlers/notification.actions';
+import { UserActions } from '../../actionHandlers/user.actions';
+import { APP_STORES } from '../../app.stores';
 import { PaymentTrxModalComponent } from './payment-trx-modal.component';
 
 describe('PaymentTrxModalComponent', () => {
   let component: PaymentTrxModalComponent;
   let fixture: ComponentFixture<PaymentTrxModalComponent>;
+  let debugEl: DebugElement;
+  let _notificationActions: NotificationActions;
+  let _appStateActions: AppStateActions;
+  let _userActions: UserActions;
+
+  class MockAppStateActions {
+
+  }
+
+  class MockUserActions {
+    public processPaymentTrx(paymentReq): void {
+
+    }
+  }
+
+  class MockNotificationActions {
+    public notifyError({ type, title, message }: { type: string, title: string, message: string }): void {
+
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentTrxModalComponent ]
-    })
-    .compileComponents();
+      declarations: [ PaymentTrxModalComponent ],
+      imports: [
+        FormsModule,
+        StoreModule.provideStore(APP_STORES)
+      ],
+      providers: [
+        { provide: UserActions, useClass: MockUserActions },
+        { provide: NotificationActions, useClass: MockNotificationActions },
+        { provide: AppStateActions, useClass: MockAppStateActions }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
