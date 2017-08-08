ed5a3c2ee88562d6e47b305226bdcc1d145ea58f
Hassan Masroor <Hassan.Masroor@me.com>
2017-08-08 10:27:39 -0400

    resolved test issue for app-footer component, actual tests expected soon

--- src/app/components/app-footer/app-footer.component.spec.ts
+++ src/app/components/app-footer/app-footer.component.spec.ts
@@ -1,21 +1,29 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Store, StoreModule } from '@ngrx/store';
 
 import { AppFooterComponent } from './app-footer.component';
+import { APP_STORES } from '../../app.stores';
 
 describe('AppFooterComponent', () => {
   let component: AppFooterComponent;
   let fixture: ComponentFixture<AppFooterComponent>;
+  let debugEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppFooterComponent ]
-    })
-    .compileComponents();
+      declarations: [ AppFooterComponent ],
+      imports: [
+        StoreModule.provideStore(APP_STORES)
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppFooterComponent);
     component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
     fixture.detectChanges();
   });
 
